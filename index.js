15539d58a05f8c3c51e6bc3d08ac17604127798c
Somya Agarwal <100855860+Somya0112@users.noreply.github.com>
2023-09-26 12:40:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; 
+const API_KEY = "YOUR API_KEY"; 
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
